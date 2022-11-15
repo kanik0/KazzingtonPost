80dab865f7866b080de88b5cbefdb3efedc30f39
Massimo Bedini <massimo.bedini@avanade.com>
2022-11-15 09:06:00 +0100

    Version bump - 0.7.2

--- Views.fs
+++ Views.fs
@@ -46,7 +46,7 @@ let partial () =
 let index () =
     [
         partial()
-        let KP_VERSION = "0.7.1"
+        let KP_VERSION = "0.7.2"
 
         div [ _class "post-content container"; _style "text-align: center;"] [
             br []
